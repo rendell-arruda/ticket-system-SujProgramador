04bd060001b33a7053bde35667bce2564d2945f6
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-17 19:50:37 -0300

    conexao com firebase

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,42 @@
+import logo from '../../assets/logo.png';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
 export default function SignUp() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
-    <div>
-      <h1>Page Cadastro</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
+        </div>
+        <form>
+          <h1>Cadastre-se</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/">Jaá possui uma conta? Faça Login</Link>
+      </div>
     </div>
   );
 }
---
+++ src/services/firebaseConnection.js
@@ -0,0 +1,26 @@
+import { initialize } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyBq5CopaRsMXIBX-6jT_JoQWaDI5Bl-CV4',
+  authDomain: 'tickets-65601.firebaseapp.com',
+  projectId: 'tickets-65601',
+  storageBucket: 'tickets-65601.appspot.com',
+  messagingSenderId: '699251298393',
+  appId: '1:699251298393:web:528223bb4ea71f16ab2148',
+  measurementId: 'G-5JWS16ELNN'
+};
+
+const firebaseApp = initialize(firebaseConfig);
+//conexao com autenticaçao
+const auth = getAuth(firebaseApp);
+
+//conexao com banco
+const db = getFirestore(firebaseApp);
+
+//conexao com storega
+const storage = getStorage(firebaseApp);
+
+export { auth, db, storage };
