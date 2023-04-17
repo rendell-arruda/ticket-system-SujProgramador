7ece9706ec1166fe4a611e460cf77389b7f2bf9f
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-17 18:31:52 -0300

    pagina de login

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './signin.css';
+import logo from '../../assets/logo.png';
+
+export default function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema de chamados" />
+        </div>
+        <form>
+          <h1>Entrar</h1>
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="******"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+
+          <button type="submit">Acessar</button>
+        </form>
+        <Link to="/register">Criar uma conta</Link>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,7 @@
+export default function SignUp() {
+  return (
+    <div>
+      <h1>Page Cadastro</h1>
+    </div>
+  );
+}
