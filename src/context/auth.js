c068ef4776b7351afd90f34666facdb410aca0b1
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-18 20:22:38 -0300

    add signIn

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,6 +1,9 @@
 import { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -12,58 +15,75 @@ export const AuthContext = createContext({});
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
   //colocar um loading
-  const [loadingAuth, setLoandingAuth] = useState(false);
-  const navigate = useNavigate()
+  const [loadingAuth, setLoadingAuth] = useState(false);
+  const navigate = useNavigate();
 
-  function signIn(email, password) {
-    console.log(email);
-    console.log(password);
-    alert('logado com sucesso');
+  async function signIn(email, password) {
+    setLoadingAuth(true);
+    await signInWithEmailAndPassword(auth, email, password)
+      //pega as informacoes do usuario no banco
+      .then(async value => {
+        let uid = value.user.uid;
+
+        const docRef = doc(db, 'users', uid);
+        const docSnap = await getDoc(docRef);
+
+        let data = {
+          uid: uid,
+          nome: docSnap.data().nome,
+          email: value.user.email,
+          avatarUrl: docSnap.data().avatarUrl
+        };
+
+        setUser(data);
+        storageUser(data);
+        setLoadingAuth(false);
+        toast.success('Bem vindo de volta!');
+        navigate('/dashboard');
+      })
+      .catch(error => {
+        console.log(error);
+        setLoadingAuth(false);
+        toast.error('Opss algo deu errado');
+      });
   }
 
   //cadastar um novo user
   async function signUp(name, email, password) {
-    setLoandingAuth(true);
+    setLoadingAuth(true);
     await createUserWithEmailAndPassword(auth, email, password)
-
       .then(async value => {
-        let uid = value.user.uid
+        let uid = value.user.uid;
         await setDoc(doc(db, 'users', uid), {
           //o que eu quero passar para o banco
           nome: name,
-          avatarUrl: null,
-        })
-          .then(() => {
-            let data = {
-              uid: uid,
-              nome: name,
-              email: value.user.email,
-              avatarUrl: null
-            }
+          avatarUrl: null
+        }).then(() => {
+          let data = {
+            uid: uid,
+            nome: name,
+            email: value.user.email,
+            avatarUrl: null
+          };
 
-            //passa os dados para o usuario
-            setUser(data);
-            //salva o data no localstorage
-            storageUser(data)
-            setLoandingAuth(false);
-            navigate('/dashboard')
-            toast.success('Seja bem vindo ao sistema')
-
-
-          })
-
-      }
-      )
-      .catch((error) => {
-        console.log(error)
-        setLoandingAuth(false)
+          //passa os dados para o usuario
+          setUser(data);
+          //salva o data no localstorage
+          storageUser(data);
+          setLoadingAuth(false);
+          navigate('/dashboard');
+          toast.success('Seja bem vindo ao sistema');
+        });
+      })
+      .catch(error => {
+        console.log(error);
+        setLoadingAuth(false);
       });
   }
 
   function storageUser(data) {
-    localStorage.setItem('@ticketsPRO', JSON.stringify(data))
+    localStorage.setItem('@ticketsPRO', JSON.stringify(data));
   }
-
 
   //compartilha as informaçoes com a aplicação inteira
   return (
@@ -72,7 +92,8 @@ export default function AuthProvider({ children }) {
         signed: !!user, //false por padrao
         user,
         signIn,
-        signUp, loadingAuth
+        signUp,
+        loadingAuth
       }}
     >
       {children}
