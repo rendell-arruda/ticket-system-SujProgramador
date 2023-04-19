2dcdf4a633f4dfdb3435d3239f35125aba7b5767
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-18 21:44:29 -0300

    uso do Private para garantir usuario logado

--- src/context/auth.js
+++ src/context/auth.js
@@ -2,7 +2,8 @@ import { useState, createContext, useEffect } from 'react';
 import { auth, db } from '../services/firebaseConnection';
 import {
   createUserWithEmailAndPassword,
-  signInWithEmailAndPassword
+  signInWithEmailAndPassword,
+  signOut
 } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { useNavigate } from 'react-router-dom';
@@ -14,9 +15,21 @@ export const AuthContext = createContext({});
 //cria provedor de contexto
 export default function AuthProvider({ children }) {
   const [user, setUser] = useState(null);
-  //colocar um loading
   const [loadingAuth, setLoadingAuth] = useState(false);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    async function loadUser() {
+      const storageUser = localStorage.getItem('@ticketsPRO');
+      if (storageUser) {
+        setUser(JSON.parse(storageUser));
+        setLoading(false);
+      }
+      setLoading(false);
+    }
+    loadUser();
+  }, []);
 
   async function signIn(email, password) {
     setLoadingAuth(true);
@@ -85,6 +98,12 @@ export default function AuthProvider({ children }) {
     localStorage.setItem('@ticketsPRO', JSON.stringify(data));
   }
 
+  async function logout() {
+    await signOut(auth);
+    localStorage.removeItem('@ticketsPRO');
+    setUser(null);
+  }
+
   //compartilha as informaçoes com a aplicação inteira
   return (
     <AuthContext.Provider
@@ -93,7 +112,9 @@ export default function AuthProvider({ children }) {
         user,
         signIn,
         signUp,
-        loadingAuth
+        logout,
+        loadingAuth,
+        loading
       }}
     >
       {children}
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
+import { AuthContext } from '../../context/auth';
+import { useContext } from 'react';
 
 export default function Dashboard() {
-  return <div>Tela de Dashboard</div>;
+  const { logout } = useContext(AuthContext);
+
+  async function handleLogout() {
+    await logout();
+  }
+  return (
+    <div>
+      <h1>Tela de Dashboard</h1>
+      <button onClick={handleLogout}>Sair da conta</button>
+    </div>
+  );
 }
