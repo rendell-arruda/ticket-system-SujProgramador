847ab1109c7566c7fb2e971ca496201478187cad
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-22 11:52:39 -0300

    create profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import './profile.css';
 import Header from '../../components/Header';
 import Title from '../../components/Title';
-import { FiSettings } from 'react-icons/fi';
+import { FiSettings, FiUpload } from 'react-icons/fi';
+import avatar from '../../assets/avatar.png';
+import { AuthContext } from '../../context/auth';
+
 export default function Profile() {
+  const { user } = useContext(AuthContext);
+  const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl);
   return (
     <div>
       <Header />
@@ -10,8 +16,40 @@ export default function Profile() {
         <Title name="Minha conta">
           <FiSettings size={25} />
         </Title>
+        <div className="container">
+          <form className="form-profile">
+            <label className="label-avatar">
+              <span>
+                <FiUpload color="#FFF" size={25} />
+              </span>
+              <input type="file" accept="image/*" /> <br />
+              {avatarUrl === null ? (
+                <img
+                  src={avatar}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              ) : (
+                <img
+                  src={avatarUrl}
+                  alt="Foto de perfil"
+                  width={250}
+                  height={250}
+                />
+              )}
+            </label>
+            <label>Nome</label>
+            <input type="text" placeholder="Seu nome" />
+            <label>Email</label>
+            <input type="email" placeholder="email@email.com" disabled={true} />
+            <button type="submit"> Salvar</button>
+          </form>
+        </div>
+        <div className="container">
+          <button className="logout-btn">Sair</button>
+        </div>
       </div>
-      <h1>Perfil</h1>
     </div>
   );
 }
