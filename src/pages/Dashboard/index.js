915b40f0e60564f81183bed666e667c0979ba5f1
Rendell Arruda <rendellarruda3@gmail.com>
2023-04-25 19:15:35 -0300

    termina tabel

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -34,11 +34,16 @@ export default function Dashboard() {
                 <th scope="col">#</th>
               </tr>
             </thead>
+
             <tbody>
               <tr>
-                <td data-label="Cliente">Mercado Esquina</td>
+                <td data-label="Cliente">Informática techs Esquina</td>
                 <td data-label="Assunto">Suporte</td>
-                <td data-label="Status">Em aberto</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: '#999' }}>
+                    Em aberto
+                  </span>
+                </td>
                 <td data-label="Cadastrado">12/06/22</td>
                 <td data-label="#">
                   <button
@@ -47,7 +52,35 @@ export default function Dashboard() {
                   >
                     <FiSearch color="#fff" size={17} />
                   </button>
-                  <button style={{ backgroundColor: '#f6a935' }}>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#f6a935' }}
+                  >
+                    <FiEdit2 color="#fff" size={17} />
+                  </button>
+                </td>
+              </tr>
+
+              <tr>
+                <td data-label="Cliente">Mercado Esquina</td>
+                <td data-label="Assunto">Suporte</td>
+                <td data-label="Status">
+                  <span className="badge" style={{ backgroundColor: '#999' }}>
+                    Em aberto
+                  </span>
+                </td>
+                <td data-label="Cadastrado">12/06/22</td>
+                <td data-label="#">
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#3583f6' }}
+                  >
+                    <FiSearch color="#fff" size={17} />
+                  </button>
+                  <button
+                    className="action"
+                    style={{ backgroundColor: '#f6a935' }}
+                  >
                     <FiEdit2 color="#fff" size={17} />
                   </button>
                 </td>
